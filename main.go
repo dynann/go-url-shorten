baeb475624cabf168ce0699f4366bc845c7b8d8c
dynann <dynannran@gmail.com>
2025-09-30 09:55:36 +0700

    Update main.go

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func main() {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000", "http://localhost:8080", "https://url-shortening-beryl.vercel.app"},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPut, http.MethodOptions, http.MethodPatch},
-		// AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		AllowHeaders: []string{"*"},
 	}))
 	e.Logger.Fatal(e.Start(":"+PORT))
